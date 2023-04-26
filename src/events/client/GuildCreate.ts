d976519182b80b1a7f8e10bbc363b65fc77e19af
JohnDavid <76064400+JohnDavid30@users.noreply.github.com>
2023-04-26 20:26:40 +0700

    Add files via upload

---
+++ src/events/client/GuildCreate.ts
@@ -0,0 +1,55 @@
+import { Guild } from '@prisma/client';
+import { Event, RoyalBot, Context } from '../../structures/index.js';
+import { WebhookClient } from 'discord.js';
+export default class GuildCreate extends Event {
+  constructor(client: RoyalBot, file: string) {
+    super(client, file, {
+      name: 'guildCreate',
+    });
+  }
+  public async run(guild): Promise<void> {
+
+    const hook = new WebhookClient({ url: this.client.config.hooks.guildAdd.url });
+        if (!hook) return;
+
+        const embed = this.client.embed().setColor(this.client.color.main).setAuthor({ name: `${this.client.user.username} has been added to a guild.`, iconURL: guild.iconURL({}) }).setTitle(`${guild.name ? guild.name : 'unknowm'}`).setThumbnail(guild.iconURL({})).addFields([
+            {
+                name: 'Created On',
+                value: `<t:${Math.round(guild.createdTimestamp / 1000)}>`,
+                inline: false,
+            },
+            {
+                name: 'Added On',
+                value: `<t:${Math.round(Date.now() / 1000)}>`,
+                inline: false,
+            },
+            {
+                name: 'Guild Id',
+                value: `\`${guild.id}\``,
+                inline: false,
+            },
+            {
+                name: 'Owner',
+                value: `<@${guild.ownerId}> (\`id: ${guild.ownerId}\`)`,
+                inline: false,
+            },
+            {
+                name: 'Total Members Count',
+                value: `\`[ ${guild.memberCount} ]\``,
+                inline: false,
+            },
+        ]);
+
+        await hook.send({ embeds: [embed] }).catch(() => { });
+  }
+}
+
+/**
+ * Project: royal-bot
+ * Author: JohnDavid
+ * Company: Mocy-Studio
+ * Copyright (c) 2023. All rights reserved.
+ * This code is the property of Mocy and may not be reproduced or
+ * modified without permission. For more information, contact us at
+ * https://discord.gg/kpHn8Nh8p3
+ */
